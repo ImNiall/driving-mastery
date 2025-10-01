35d5c3247c7d83a89cef804be2e03cdfbea2e6cf
Niall Cullen <niallcullen.business@gmail.com>
2025-10-01 11:23:08 +0100

    fix(auth): fallback ClerkJS to jsDelivr CDN

--- index.tsx
+++ index.tsx
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <ClerkProvider
       publishableKey={(import.meta as any).env?.VITE_CLERK_PUBLISHABLE_KEY as string}
-      clerkJSUrl="https://cdn.clerk.com/npm/@clerk/clerk-js@5/dist/clerk.browser.js"
+      clerkJSUrl="https://cdn.jsdelivr.net/npm/@clerk/clerk-js@5/dist/clerk.browser.js"
     >
       <App />
     </ClerkProvider>
